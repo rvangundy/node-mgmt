360eaf5257d7cd30d3d3524f3de2cfacabf56937
Ryan VanGundy <ryanvangundy@Ryans-MacBook-Pro.local>
2014-03-08 23:13:42 -0500

    Add develop task
    #gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
 'use strict';
 
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+/******************
+ *  Dependencies  *
+ ******************/
 
+var gulp       = require('gulp');
+var mocha      = require('gulp-mocha');
+var supervisor = require('gulp-supervisor');
+
+/***********
+ *  Tasks  *
+ ***********/
+
+// Develop
+gulp.task('develop', function () {
+    supervisor('./server/server.js', {
+       // args         : [ 'dev' ],
+        watch        : ['server', 'app', 'src', 'templates'],
+        ignore       : null,
+        pollInterval : 500,
+        extensions   : ['js,hbs,styl'],
+        exec         : 'node',
+        debug        : true,
+        debugBrk     : false,
+        harmony      : false,
+        noRestartOn  : 'error',
+        forceWatch   : true,
+        quiet        : false
+    });
+});
+
+// Test
 gulp.task('test', function () {
     gulp.src('test/index.js')
         .pipe(mocha());
